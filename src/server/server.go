8eb3c038aea3a0dd6d4770ffa8f86580ecbf2eb3
justincremer <jacremer@live.com>
2021-04-02 06:14:12 -0700

    broke out router

--- src/server/server.go
+++ src/server/server.go
@@ -4,33 +4,23 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/justincremer/go-orm/src/book"
 	"github.com/justincremer/go-orm/src/database"
+	"github.com/justincremer/go-orm/src/models/book"
+	"github.com/justincremer/go-orm/src/models/user"
 )
-
-func welcome(c *fiber.Ctx) error {
-	return c.SendString("Welcome from http://localhost:8080")
-}
-
-func router(app *fiber.App) {
-	app.Get("/", welcome)
-	app.Get("/book", book.ListBooks)
-	app.Get("/book/:id", book.GetBook)
-	app.Post("/book", book.CreateBook)
-	app.Patch("/book/:id", book.UpdateBook)
-	app.Delete("/book/:id", book.DeleteBook)
-}
 
 func Create(port string, config fiber.Config) *fiber.App {
 	app := fiber.New(config)
-
 	dsn := "host=localhost user=admin password=admin dbname=admin port=5432 sslmode=disable TimeZone=America/Los_Angeles"
 	conn := database.Connect(dsn)
 
 	conn.AutoMigrate(&book.Book{})
+	conn.AutoMigrate(&user.User{})
 	fmt.Println("Migration successful")
 
-	router(app)
+	app.Get("/", welcome)
+	bookRouter(app)
+	userRouter(app)
 
 	return app
 }
